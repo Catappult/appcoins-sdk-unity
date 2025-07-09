c35c8569fd8956f8fe7da491e48831bb074ffed3
SergiyKhomynAPT <sergiy.khomyn@aptoide.com>
2025-07-09 11:46:52 +0100

    Fix compiling errors.

--- Runtime/SDK/ProductDetails.cs
+++ Runtime/SDK/ProductDetails.cs
@@ -10,53 +10,53 @@ public class ProductDetails
     public string Description;
     public OneTimePurchaseOfferDetails OneTimePurchaseOfferDetails;
     public List<SubscriptionOfferDetails> SubscriptionOfferDetails;
+}
 
-    [System.Serializable]
-    public class OneTimePurchaseOfferDetails
-    {
-        public string FormattedPrice;
-        public long PriceAmountMicros;
-        public string PriceCurrencyCode;
-        public string AppcFormattedPrice;
-        public long AppcPriceAmountMicros;
-        public string AppcPriceCurrencyCode;
-        public string FiatFormattedPrice;
-        public long FiatPriceAmountMicros;
-        public string FiatPriceCurrencyCode;
-    }
+[System.Serializable]
+public class OneTimePurchaseOfferDetails
+{
+    public string FormattedPrice;
+    public long PriceAmountMicros;
+    public string PriceCurrencyCode;
+    public string AppcFormattedPrice;
+    public long AppcPriceAmountMicros;
+    public string AppcPriceCurrencyCode;
+    public string FiatFormattedPrice;
+    public long FiatPriceAmountMicros;
+    public string FiatPriceCurrencyCode;
+}
 
-    [System.Serializable]
-    public class SubscriptionOfferDetails
-    {
-        public PricingPhases PricingPhases;
-        public TrialDetails TrialDetails;
-    }
+[System.Serializable]
+public class SubscriptionOfferDetails
+{
+    public PricingPhases PricingPhases;
+    public TrialDetails TrialDetails;
+}
 
-    [System.Serializable]
-    public class PricingPhases
-    {
-        public List<PricingPhase> PricingPhaseList;
-    }
+[System.Serializable]
+public class PricingPhases
+{
+    public List<PricingPhase> PricingPhaseList;
+}
 
-    [System.Serializable]
-    public class PricingPhase
-    {
-        public string BillingPeriod;
-        public string FormattedPrice;
-        public long PriceAmountMicros;
-        public string PriceCurrencyCode;
-        public string AppcFormattedPrice;
-        public long AppcPriceAmountMicros;
-        public string AppcPriceCurrencyCode;
-        public string FiatFormattedPrice;
-        public long FiatPriceAmountMicros;
-        public string FiatPriceCurrencyCode;
-    }
+[System.Serializable]
+public class PricingPhase
+{
+    public string BillingPeriod;
+    public string FormattedPrice;
+    public long PriceAmountMicros;
+    public string PriceCurrencyCode;
+    public string AppcFormattedPrice;
+    public long AppcPriceAmountMicros;
+    public string AppcPriceCurrencyCode;
+    public string FiatFormattedPrice;
+    public long FiatPriceAmountMicros;
+    public string FiatPriceCurrencyCode;
+}
 
-    [System.Serializable]
-    public class TrialDetails
-    {
-        public string Period;
-        public string PeriodEndDate;
-    }
+[System.Serializable]
+public class TrialDetails
+{
+    public string Period;
+    public string PeriodEndDate;
 }
