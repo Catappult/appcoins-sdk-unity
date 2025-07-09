4ccad1822df9c5a57e4ffa08170c0fe4b4873dc1
SergiyKhomynAPT <sergiy.khomyn@aptoide.com>
2025-07-09 11:07:35 +0100

    Fix compiling errors.

--- Runtime/SDK/BillingResult.cs
+++ Runtime/SDK/BillingResult.cs
@@ -1,49 +1,6 @@
-using System;
-
 [System.Serializable]
 public class BillingResult
 {
-    public int? ResponseCode;
-    public string DebugMessage;
-
-    internal BillingResult(int? responseCode = null, string debugMessage = "")
-    {
-        ResponseCode = responseCode;
-        DebugMessage = debugMessage;
-    }
-
-    public override string ToString()
-    {
-        return $"ResponseCode: {ResponseCode}, DebugMessage: {DebugMessage}";
-    }
-
-    public class Builder
-    {
-        private int? responseCode = null;
-        private string debugMessage = "";
-
-        internal Builder() { }
-
-        public Builder SetResponseCode(int responseCode)
-        {
-            this.responseCode = responseCode;
-            return this;
-        }
-
-        public Builder SetDebugMessage(string debugMessage)
-        {
-            this.debugMessage = debugMessage;
-            return this;
-        }
-
-        public BillingResult Build()
-        {
-            return new BillingResult(responseCode, debugMessage);
-        }
-    }
-
-    public static Builder NewBuilder()
-    {
-        return new Builder();
-    }
+    public int? responseCode;
+    public string debugMessage;
 }
