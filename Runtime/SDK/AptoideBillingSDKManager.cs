dd94c3b0d54ceaa88ba63c221d49cf787dc7158d
SergiyKhomynAPT <sergiy.khomyn@aptoide.com>
2025-07-09 11:19:42 +0100

    Fix compiling errors.

--- Runtime/SDK/AptoideBillingSDKManager.cs
+++ Runtime/SDK/AptoideBillingSDKManager.cs
@@ -241,8 +241,8 @@ public class AptoideBillingSDKManager : MonoBehaviour
         Debug.Log($"AptoideBillingSDKManager | Product Details Received: {productDetailsResultJson}");
 
         ProductDetailsResult productDetailsResult = JsonUtility.FromJson<ProductDetailsResult>(productDetailsResultJson);
-        Debug.Log($"AptoideBillingSDKManager | Product Details Received 1: {productDetailsResult.billingResult}");
-        Debug.Log($"AptoideBillingSDKManager | Product Details Received 2: {productDetailsResult.details}");
+        Debug.Log($"AptoideBillingSDKManager | Product Details Received 1: {productDetailsResult.billingResult.responseCode} {productDetailsResult.billingResult.debugMessage}");
+        Debug.Log($"AptoideBillingSDKManager | Product Details Received 2: {productDetailsResult.details[0]}");
 
         productDetailsResponseListener.OnProductDetailsResponse(productDetailsResult.billingResult, productDetailsResult.details);
     }
